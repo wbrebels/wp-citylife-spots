73a143e7b1227ac83630463c135860ec5a7eaf57
kennybrijs <kennybrijs@gmail.com>
2013-09-20 14:30:10 +0200

    Display posts in rows of 3

--- list-location.php
+++ list-location.php
@@ -14,25 +14,38 @@ get_header(); ?>
 			$mypost = array( 'post_type' => 'location' );
 			$loop = new WP_Query( $mypost );
 
+			$post_number = 1;
+			$posts_total = $loop->found_posts;
+
 			while ( $loop->have_posts() ) : $loop->the_post(); 
 				
 				?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
 
-						<h1 class="entry-title">
-							<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-						</h1>
-						<a href="<?php the_permalink(); ?>" rel="bookmark">
-							<?php the_post_thumbnail('medium'); ?>
-						</a>
-					</header>
-						
-				</article>
+				<?php if($post_number % 3 == 1){ ?>
+                <div class="row">
+                <?php } ?>
 
-				<hr/>
+					<div class="span3">
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header class="entry-header">
+
+								<h1 class="entry-title">
+									<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+								</h1>
+								<a href="<?php the_permalink(); ?>" rel="bookmark">
+									<?php the_post_thumbnail('medium'); ?>
+								</a>
+							</header>
+								
+						</article>
+					</div>
+
+				<?php if($post_number % 3 == 0 or $post_number == $posts_total){ ?>
+                </div>
+                <?php } ?>
+
 				<?php 
-
+				$post_number++;
 			endwhile; 
 		?>
 	</div>
